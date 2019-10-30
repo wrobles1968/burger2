06b3115a90be906e88c24def97449e100e542d37
Walter Robles <robleswalter1968@gmail.com>
2019-10-30 11:56:07 -0700

    push file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3306;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
